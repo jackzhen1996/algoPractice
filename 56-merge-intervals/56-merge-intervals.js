2a744a9a8db2d0178bbd406a032059934cd4e763
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-03-11 21:48:07 -0800

    Time: 159 ms (53.59%), Space: 48.4 MB (71.19%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -9,7 +9,7 @@ var merge = function(intervals) {
     }
     
     intervals = intervals.sort((a,b)=>a[0]-b[0])
-    const mergedIntervals = []
+    // const mergedIntervals = []
     
     
     // assume intervals are sorted from smallest to largest by starting point
@@ -22,24 +22,47 @@ var merge = function(intervals) {
         // determine if current two intervals have overlap
             // if so, reassign biggest interval with the current interval
     // end of inner loop: push the current biggest interval to the answer 
-    let i = 0
-    while (i <= intervals.length - 1) {
-        let j = i + 1
-        let k = 0
-        let currentBiggest = intervals[i]
-        while (j <= intervals.length - 1) {
-            if (currentBiggest[1] >= intervals[j][0]) {
-                // overlap
-                currentBiggest = [currentBiggest[0], Math.max(intervals[j][1], currentBiggest[1])]
-                k = j
-            } 
-            j++
+    
+   //  my way
+//     let i = 0
+
+//     while (i <= intervals.length - 1) {
+//         let j = i + 1
+//         let k = 0
+//         let currentBiggest = intervals[i]
+//         while (j <= intervals.length - 1) {
+//             if (currentBiggest[1] >= intervals[j][0]) {
+//                 // overlap
+//                 currentBiggest = [currentBiggest[0], Math.max(intervals[j][1], currentBiggest[1])]
+//                 k = j
+//             } 
+//             j++
+//         }
+//         mergedIntervals.push(currentBiggest)
+//         if (k > 0) {
+//             i = k
+//         }
+//         i++
+        
+//     }
+    
+    // one pass way
+    // keep track of lastInterval
+    // iterate the interval
+        // if overlap: change the make sure the lastInterval captures the largest interval
+        // if not overlap: add the interval to answer, reassign interval because we're at a new interval
+    
+    const mergedIntervals = [intervals[0]]
+    
+    for (let i = 1; i < intervals.length; i++) {
+        const lastEnd = mergedIntervals[mergedIntervals.length-1][1]
+        
+        // overlaps
+        if (lastEnd >= intervals[i][0]) {
+            mergedIntervals[mergedIntervals.length-1][1] = Math.max(mergedIntervals[mergedIntervals.length-1][1], intervals[i][1])
+        } else {
+            mergedIntervals.push(intervals[i])
         }
-        mergedIntervals.push(currentBiggest)
-        if (k > 0) {
-            i = k
-        }
-        i++
     }
     
     return mergedIntervals
