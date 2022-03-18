a3b3ee5b8daf07614767ebb3059d30406d57b344
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-03-17 22:04:56 -0700

    Time: 256 ms (5.17%), Space: 50.4 MB (5.62%) - LeetHub

---
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.js
@@ -0,0 +1,54 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @param {string[]} sources
+ * @param {string[]} targets
+ * @return {string}
+ */
+var findReplaceString = function(s, indices, sources, targets) {
+    // notes
+    // whatever substring at indices[i] must equal to substring form sources
+    // we dont want to mess up the indexing
+        // splitting up the string into indices.length pieces
+        // use sources array to determine which part we didn't split
+        // join back up substrings together at the end
+        
+    // abcd
+    // a, b ,cd 
+    // a => eee
+    // b => b
+    // cd => ffff
+    // answer = eeebfff
+    
+    // {0: {source: 'a', target: 'eee'}, 2: {source: 'cd', target: 'ffff'}}
+    
+    // O (k), k is length of indices, sources, target
+    const mapIndicesToSources = {}
+    for (let i = 0; i < indices.length; i++) {
+        mapIndicesToSources[indices[i]] = {source: sources[i], target: targets[i]}
+    }
+    
+    const substrings = []
+    // O(n), n = s.length
+    for (let i = 0; i < s.length; i++) {
+        console.log(i)
+        if (mapIndicesToSources[i]) {
+            // check if substring in sources == substring here
+            // construct the new substring
+            const {source, target} = mapIndicesToSources[i]
+            const sliceEndIndex = i + source.length
+            const slice = s.slice(i, sliceEndIndex)
+            if (slice === source) {
+                substrings.push(target)
+            } else {
+                substrings.push(slice)
+            }
+            i = sliceEndIndex - 1
+        } else {
+            substrings.push(s[i])
+        }
+    }
+    console.log({substrings, mapIndicesToSources})
+    // O(n + k)
+    return substrings.join('')
+};
