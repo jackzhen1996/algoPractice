21e0ea8adefbb2a984d7c7ce837f020c2182205d
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-03-20 17:16:22 -0700

    Time: 829 ms (6.47%), Space: 48.4 MB (21.85%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    // edge case: k is 0, means return longest repeating for s 
+    
+    // abaaa => sliding window
+    // for current window
+        // update hash
+        // if window length - most frequent char count <= k, that means we have enough replacements to get the longest repeating substring
+            // move j pointer forward
+        // if not, move i pointer forward, and delete the count of the letter at i in hash
+    
+    let i = 0 
+    let j = 0
+    let max = 0
+    let maxOcc = 0
+    
+    const hash = {}
+    // letter to counts
+    
+    while (j <= s.length -1 ) {
+        hash[s[j]] ? hash[s[j]] ++ : hash[s[j]] = 1
+        
+        while (j - i + 1 - Math.max(...Object.values(hash)) > k) {
+            hash[s[i]] --
+            i ++
+        }
+        
+        max = Math.max(max, j - i + 1 )
+        j ++ 
+    }
+    
+    return max
+};
