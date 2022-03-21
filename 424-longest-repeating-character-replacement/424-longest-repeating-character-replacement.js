f04b17f8f208ecfff886a81513640750e8da56a9
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-03-20 17:19:19 -0700

    Time: 452 ms (14.22%), Space: 48.7 MB (12.41%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -26,9 +26,11 @@ var characterReplacement = function(s, k) {
         hash[s[j]] ? hash[s[j]] ++ : hash[s[j]] = 1
         
         // if we encounter an invalid window size where replacements > max replacements, we shrink the window, and delete char from ith place
+        maxOcc = Math.max(hash[s[j]], maxOcc )
         while (j - i + 1 - Math.max(...Object.values(hash)) > k) {
             hash[s[i]] --
             i ++
+            maxOcc = Math.max(hash[s[j]], maxOcc )
         }
         
         // re-evaluate max
