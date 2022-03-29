470da0541fa12ab948396d2efcd47ec451c6b7e9
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-03-28 21:37:49 -0700

    Time: 80 ms (95.89%), Space: 50.8 MB (59.88%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -4,15 +4,6 @@
  */
 var containsDuplicate = function(nums) {
     // use hashset to see if nums[i] already exist
-    
-    const hashset = new Set()
-    
-    for (const num of nums) {
-        if (hashset.has(num)) {
-            return true
-        } 
-        hashset.add(num)
-    }
-    
-    return false
+     
+    return (new Set(nums)).size !== nums.length
 };
