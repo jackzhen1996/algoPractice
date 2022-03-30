b185f853f9e958d0331c679f82fde994b47166c2
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-03-29 22:21:54 -0700

    Time: 154 ms (43.05%), Space: 54.8 MB (46.40%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,20 +3,42 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    // brute force
-    // outer loop: anchor on a number, i
-        // inner loop: iterate through other numbers, skip the ith number
+    // calculate prefix array
+    // calculate postfix array
+    // multiply prefix with postfix
     
-    const answer = []
-    for (let i = 0; i < nums.length; i++) {
-        let product = 1
-        for (let j = 0; j < nums.length; j++) {
-            if (j !== i) {
-                product *= nums[j]
-            }
+    const prefix = []
+    const postfix = new Array(nums.length)
+    
+   let prefixProd = nums[0]
+   prefix[0] = prefixProd
+   for (let i = 1; i < nums.length; i++) {
+       prefixProd *= nums[i]
+       prefix.push(prefixProd)
+   } 
+    
+  let postfixProd = nums[nums.length-1]
+  postfix[postfix.length-1] = postfixProd
+  for (let i = nums.length-2; i >= 0; i--) {
+      postfixProd *= nums[i]
+      postfix[i] = postfixProd
+  }
+    
+   // prefix [1,2,6,12]
+   // postfix [24,24,12,4]
+   // answer [24,12,8,6]
+   // iterate through from 0 to nums.length- 1
+    // multiply prefix[i-1] * postfix[i+1]
+    console.log(prefix, postfix)
+    const answer = [postfix[1]]
+    for (let i = 1; i < nums.length; i++) {
+        if (postfix[i+1] == null) {
+            answer.push(prefix[i-1])
+        } else {
+            answer.push(prefix[i-1]*postfix[i+1])
         }
-        answer.push(product)
     }
     
     return answer
+  
 };
