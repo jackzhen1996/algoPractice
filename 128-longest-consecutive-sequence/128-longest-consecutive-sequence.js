4501570473b933c1bf93baa288dcd9ba7b0b7ef4
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-04-03 15:01:03 -0700

    Time: 649 ms (15.94%), Space: 51.2 MB (35.93%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -3,31 +3,22 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    // brute force:
-    // sort the array O(nlogn)
-    // anchor one element, search elements nums[i] and nums[j] + 1 => O(n^2)
+    // if there is a sequence, sequence[0] should not have left neighbor
+    // so if nums[i] has no left neighbor, we know for sure that this is the start of sequence
+        // use set => retrieve left neighbor, nums[i] - 1
     
-    // visual the ranges
-    // start of a sequence must not have a left neighbor, aka if 1 is starting, then 0 must not exist
+    // [100,4,200,1,3,2]
     
-    // convert the nums to a set, assuming nums are unique, if not unique then use a hashmap to map num to counts
-    // iterate through nums,
-        // if left neighbor exist
-            // go to next to next one
-        // if left neighbor does not exist, we know this is the beginning of sequence
-            // look for number that is one greater, keep track of the max length of sequence
-    
-    const set = new Set(nums) // contains all numbers
+    const set = new Set(nums)
     let maxLength = 0
     for (let i = 0; i < nums.length; i++) {
-        if (!set.has(nums[i] - 1)) {
-            let currentLength = 0   
-            while(set.has(nums[i]+currentLength)) {
+        if (!set.has(nums[i]-1)) {
+            let currentLength = 0
+            while (set.has(nums[i]+currentLength)) {
                 currentLength ++
                 maxLength = Math.max(currentLength, maxLength)
             }
         }
     }
-    
     return maxLength
 };
