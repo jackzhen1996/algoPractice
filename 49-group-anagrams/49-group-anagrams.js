0ea7415d160d87f6745d6d68237ce055bbc09c49
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-04-18 20:52:46 -0700

    Time: 104 ms (98.63%), Space: 52.9 MB (75.34%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -3,28 +3,24 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    // brute force
-    // sort each word
-    // put in hash, {sorted: [words that are not sorted]}
+    // hashmap: sortedWord: [group of anagrams]
+    // iterate thru strs
+        // if strs[i] sorted is not in hashmap
+            // put strs[i].sort as key, and put [strs[i]] as value
+        // if strs[i] sorted
+            // push into array
     
-    // if length 1, then just return itself
-    const answer = []
-    const sortedToWords = {}
+    // iterate thru hashmap, return the arrays of anagrams
     
+    const sortedWordToAnagrams = {}
     for (const word of strs) {
-        const sortedWord = word.split('').sort().join()
-        
-        if (sortedToWords[sortedWord]) {
-            sortedToWords[sortedWord].push(word)
+        const sortedWord = word.split('').sort().join('')
+        if (sortedWordToAnagrams[sortedWord]) {
+            sortedWordToAnagrams[sortedWord].push(word)
         } else {
-            sortedToWords[sortedWord] = [word]
+            sortedWordToAnagrams[sortedWord] = [word]
         }
     }
-
-    for (const word in sortedToWords) {
-        answer.push(sortedToWords[word])
-    }
     
-    
-    return answer
+    return Object.values(sortedWordToAnagrams)
 };
