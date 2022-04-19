35be243db609bb8581f0327bf0a5eecd0bb9f93e
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-04-18 21:59:45 -0700

    Time: 109 ms (48.50%), Space: 48.5 MB (17.58%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -4,28 +4,46 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    // collect the count of nums for each number in hashmap
-    // extra hashmap values, sort them
-    // get top k counts from hashmap values
-    
-    // iterate the hashmap again to find the actual numbers
+    // collect counts for each num, {num: countOfNum}
+    // create array where index = counts, nums[index] = [nums that have this count]
+    // since array indices are already sorted, return the top k elements
     
     const hash = {}
-    for (let i = 0; i < nums.length; i++) {
-        if (hash[nums[i]]) {
-            hash[nums[i]] ++
+    for (const num of nums) {
+        if (hash[num]) {
+            hash[num] ++
         } else {
-            hash[nums[i]] = 1
+            hash[num] = 1
         }
     }
     
-    const topKCounts = new Set(Object.values(hash).sort((a,b)=>b-a).slice(0,k))
+    const countToNums = []
+    for (const num in hash) {
+        const numsWithCount = countToNums[hash[num]]
+        if (numsWithCount) {
+            countToNums[hash[num]].push(num)
+        } else {
+            countToNums[hash[num]] = [num]
+        }
+    }
+    // [,[3],[2],[1]]
+    console.log(countToNums)
     
     const answer = []
-    for (const num in hash) {
-        if (topKCounts.has(hash[num])) {
-            answer.push(num)
+    let j = k
+    let i = countToNums.length - 1
+    while (i > 0 && j > 0) {
+        
+        if (countToNums[i]) {
+        if (countToNums[i].length <= j) {
+            answer.push(...countToNums[i])
+            j -= countToNums[i].length
+        } else {
+            const currentBucket = countToNums[i]
+            return currentBucket.slice(currentBucket.length - k)
         }
+        }
+        i--
     }
     
     return answer
