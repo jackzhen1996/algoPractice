09224d80ee26e5981b0574a28900e14faaa8a397
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-04-28 22:06:34 -0700

    Time: 112 ms (70.93%), Space: 46.6 MB (81.58%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -2,55 +2,23 @@
  * @param {string} s
  * @return {number}
  */
-
-// abcabcbb
-// begin starts at 0, end starts at 0
-// if we see non repeating, we move end pointer forward
-    // a is not repeating, a is current longest
-    // ab is not repeating, ab is current longest
-    // abc is not repeating, abc is current longest
-    // abca is repeating, abc is current longest 
-        // move beginning pointer to after the very latest repeating letter is at, move end pointer to same place
-
-// abba => ab 
-// a, ab, abb, b
 var lengthOfLongestSubstring = function(s) {
-    // iterate s
-    // keep track of current longest
-        // replace the current longest if we see another longest
+    // left pointer
+    // iterate thru s with right pointer
+        // as long as there's no repeating char, evaluate maxlength
+        // as long there is repeating char, move left pointer until repetitions are removed
     
-    // return the longest at the end
-    
-    let longest = 0
-    let start = 0
-    let end = 0
-    let uniqueLetters = new Set()
-    
-    while (end <= s.length - 1) {
-        // let currentLongest = s.substring(start,end+1)
-        // const lastLetter = currentLongest[currentLongest.length-1]
-        // if (uniqueLetters.has(lastLetter)) {
-        //     // move start and end pointer
-        //     // renew set because we're going to evaluate a different substring
-        //     uniqueLetters = new Set()
-        //     start ++ 
-        //     end = start 
-        // } else {
-        //     // move end pointer
-        //     uniqueLetters.add(lastLetter)
-        //     longest = Math.max(longest, currentLongest.length)
-        //     end ++
-        // }
-        
-        if (uniqueLetters.has(s[end])) {
-            uniqueLetters.delete(s[start])
-            start ++ 
-        } else {
-            uniqueLetters.add(s[end])
-            end ++
-            longest = Math.max(longest, uniqueLetters.size)
+    let l = 0
+    let maxLength = 0
+    const set = new Set()
+    for (let r = 0; r < s.length; r ++) {
+        const substr = s.substring(r,r+1)
+        while (set.has(substr)) {
+            set.delete(s.substring(l,l+1))
+            l ++
         }
+        set.add(substr)
+        maxLength = Math.max(maxLength, set.size)
     }
-    
-    return longest
+    return maxLength
 };
