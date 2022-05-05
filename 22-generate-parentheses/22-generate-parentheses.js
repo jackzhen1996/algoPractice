7ff481aff00264a5c0263f5e54960fa6a344d9d5
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-05-05 15:59:59 -0700

    Time: 75 ms (63.45%), Space: 44.1 MB (24.77%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    // n means n opens and n closes
+        // total of 6 items, re-arrange 6 items such that they are all valid
+    
+    // brute force: generate all possible permutations => O(n!)
+        // then go thru each permutation and validate => O(n) 
+    // Overall: O(n*n!)
+    
+    // n = 1, ()
+    // n = 3, ((())) (()()) (())() ()(()) ()()()
+    
+    const output = []
+    const dfs = function(parens, open, close) {
+        // base case
+        if(close > open || close > n || open > n)  {
+            return
+        }
+        
+        else if (open === n && close === n) {
+            output.push(parens)
+            return
+        }
+        
+        else if (open === n && close < n) {
+            dfs(parens +')', open, close+1)
+        } else {
+            dfs(parens +'(', open+1, close)
+            dfs(parens +')', open, close+1)
+        }
+        
+    }
+    
+    dfs('(', 1,0)
+    return output
+};
