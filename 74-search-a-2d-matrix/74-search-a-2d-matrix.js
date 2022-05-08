72c296d16d8b4614c0f01c76d972dabbe79d49f6
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-05-07 20:30:37 -0700

    Time: 76 ms (58.32%), Space: 42.3 MB (41.15%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -31,16 +31,67 @@ var search = function(nums, target) {
 var searchMatrix = function(matrix, target) {
     // easy way: iterate thru matrix, for each row do binary search
     // yields: O(m log n ), if there are m rows and each row has n items
-    let result = false
-    for (const row of matrix) {
-        result = search(row, target)
-        if (result) {
-            return true
-        }
-    }
-    return result
+    // let result = false
+    // for (const row of matrix) {
+    //     result = search(row, target)
+    //     if (result) {
+    //         return true
+    //     }
+    // }
+    // return result
+    
     // assume the grid is just a 1d array, then the length is m x n
     // example, 3 x 4 = 12,
         // l = 0, r = 11, mid = (l + r) /2
-        // 6 => ( row = 6 / m = 6 / 3 = 2 ) , (col = numRow x n - 6)
+        // 5 => ( row = 5 / m = 5 / 3 = 1 ) , (col = ) => [1,0]
+    // my way, didnt work for all cases
+//     let rows = matrix.length
+//     let cols = matrix[0].length
+//     let r = rows * cols - 1
+//     let l = 0
+//     while (l <= r) {
+//         let mid = Math.floor((r + l)/2)
+//         const rowIndex = Math.floor(mid/rows)
+//         const colIndex = mid - Math.floor(mid/rows)*cols
+        
+//         let midElement = matrix[rowIndex][colIndex]
+//         if (target === midElement) {
+//             return true
+//         }
+        
+//         if (target > midElement) {
+//             l = mid + 1
+//         } else {
+//             r = mid - 1
+//         }
+//     }
+//     return false
+    
+    // look at row bounds
+    // find the middle row
+        // if target > midRow[-1]: go down
+        // if target < midRow[0]: go up
+    // go down, shift topPointer to mid + 1
+    // go up, shift bottomPointer to mid - 1
+    // after binarySearch finishes, perform binary search on that row?
+    // time : O(log m) x O ( log (n) ) => O (log m + log n) => O (log n)
+    
+    let top = 0
+    let bottom = matrix.length - 1
+    let mid = null
+    while (top <= bottom) {
+        mid = Math.floor((top+bottom)/2)
+        const midRow = matrix[mid]
+        if (target <= midRow[midRow.length-1] && target >= midRow[0]) {
+            break
+        }
+        if (target > midRow[midRow.length-1]) {
+            top = mid + 1
+        } else if (target < midRow[0]) {
+            bottom = mid - 1
+        }
+    }
+    
+    return search(matrix[mid],target)
+    
 };
