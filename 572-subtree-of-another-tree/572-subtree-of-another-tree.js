76d42bf46b909b97dbd3aa30be672c871cc2aac6
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-05-18 21:50:57 -0700

    Time: 1281 ms (5.11%), Space: 52 MB (5.06%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.js
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.js
@@ -12,6 +12,8 @@
  * @return {boolean}
  */
 
+// time: O(n^2), n= num of nodes in the tree
+// space : O(1), cuz stack always only have 2 items remaining to process
 const isSameSubTree = function(a,b) {
     if (a == null && b == null) {
         return true
@@ -26,7 +28,9 @@ var isSubtree = function(root, subRoot) {
     
     const node = root
     const stack = [root]
+    // 
     while (stack.length > 0) {
+        console.log(stack)
         const popped = stack.pop()
         if (popped.val === subRoot.val) {
             // run dfs
@@ -35,12 +39,13 @@ var isSubtree = function(root, subRoot) {
                 return true
             }
         }
-        if (popped.right) {
-            stack.push(popped.right)
-        }
         
         if (popped.left) {
             stack.push(popped.left)
+        }
+        
+        if (popped.right) {
+            stack.push(popped.right)
         }
         
     }
