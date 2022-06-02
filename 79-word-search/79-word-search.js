50a002d46eeffcc2771841fa617aca7bac9e0bff
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-06-01 23:53:29 -0700

    Time: 512 ms (69.80%), Space: 42.6 MB (80.35%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -3,45 +3,44 @@
  * @param {string} word
  * @return {boolean}
  */
-
-const findWord = function(i,j,word, board, letterCount) {
-    // base case
-    if (word.length === letterCount) {
-        return true
-    }
-    
-    // recursive case
-        // does current letter match word[0]
-        // check all neighbors for next letter 
-    if (i < board.length && i >= 0 && j < board[i].length && j >= 0 && word[letterCount] === board[i][j]) {
-        const currentLetter = board[i][j]
-        board[i][j] = 0
-        const a = findWord(i-1,j, word, board, letterCount+1)
-        const b = findWord(i+1,j, word, board, letterCount+1)
-        const c = findWord(i,j-1, word, board, letterCount+1)
-        const d = findWord(i,j+1, word, board, letterCount+1)
+var exist = function(board, word) {
+    // or just save current letter..
+    const dfs = function (row,col,index) {
+        if (row > board.length -1 || col > board[0].length - 1 || row < 0 || col < 0 || word[index] !== board[row][col]) {
+            return 
+        }
         
-        if (a || b || c || d) {
+        if (index === word.length - 1) {
+            return true
+        }
+    
+        
+        const currentLetter = board[row][col]
+        board[row][col] = null
+
+        const right = dfs(row + 1, col, index + 1)
+        const left = dfs(row - 1, col, index + 1)
+        const down = dfs(row, col + 1, index + 1)
+        const up = dfs(row, col - 1, index + 1)
+        
+        if (right || left || down || up) {
             return true
         }
         
-        board[i][j] = currentLetter
+        board[row][col] = currentLetter
+        
     }
-}
-
-var exist = function(board, word) {
-    // iterate through board
-        // for each letter, find neighbors that matches the first letter 
     
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             if (board[i][j] === word[0]) {
-                if (findWord(i,j, word, board, 0)) {
+                const result = dfs(i,j,0)
+                if (result) {
                     return true
                 }
             }
         }
     }
     
-    return false 
+    return false
 };
