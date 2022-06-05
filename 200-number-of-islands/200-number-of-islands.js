7f2144e88eaaf9be94bbc62fde19737d5161d577
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-06-04 23:12:03 -0700

    Time: 88 ms (85.79%), Space: 44.8 MB (76.40%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -3,47 +3,42 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-
-    // edge case: if m and n are 1, this means we either 0 or 1.
-    if (grid.length === 1 && grid[0].length === 1) {
-        return grid[0][0]
-    }
+    // return true if 1
     
-    // recursive method
-    // iterate through the 2d array
+    // at least one of the neighbor has to be a 1
+        // left, right, up, down
+        
+    // return if 0
+    
+    // if seen 1, mark it as seen
+    
+    // increment by 1
+    
+    const dfs = function(row, col) {
+        if (row > grid.length - 1 || col > grid[0].length - 1 || row < 0 || col < 0 || grid[row][col] === '0') {
+            return
+        }
+        
+        grid[row][col] = '0'
+        
+        dfs(row + 1, col)
+        dfs(row - 1, col)
+        dfs(row, col + 1)
+        dfs(row, col - 1)
+        
+        
+    }
     
     let numIslands = 0
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[i].length; j++) {
             if (grid[i][j] === '1') {
-                numIslands += findIslands(i,j, grid)
+                numIslands++
+                dfs(i,j)
             }
         }
     }
     
     return numIslands
+    
 };
-
-const findIslands = function(i,j, grid) {
-    
-    // base case: 
-    // return if current item is either 0 or null/undefined ( use shallow equal check )
-    if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[i].length -1  || grid[i][j] === '0') {
-        return
-    }
-    
-    // 1. change the current '1' to a '0', to mark it as seen
-    // 2. check all cardinal directions
-        // 2a. up, down, left, right 
-        // call findIslands(i,j) on all immediate neighbors
-        // if all directions are checked, meaning all 1s have been turned to 0, return 1
-    
-    grid[i][j] = '0'
-    
-    findIslands(i+1,j, grid)
-    findIslands(i-1,j, grid)
-    findIslands(i,j+1, grid)
-    findIslands(i,j-1, grid)
-    
-    return 1
-}
