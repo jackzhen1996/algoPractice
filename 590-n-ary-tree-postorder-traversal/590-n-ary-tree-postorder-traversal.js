07e41cc0faf8f9555d29269d86a46efdb9beb284
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-06-20 11:04:32 -0700

    Time: 116 ms (49.37%), Space: 44.8 MB (87.34%) - LeetHub

--- 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.js
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.js
@@ -12,18 +12,29 @@
  */
 var postorder = function(root) {
     const result = []
-    const dfs = function(node) {
-        if (node == null) {
-            return
-        }
+//     const dfs = function(node) {
+//         if (node == null) {
+//             return
+//         }
         
-        for (const child of node.children) {
-            dfs(child)
-        }
+//         for (const child of node.children) {
+//             dfs(child)
+//         }
         
-        result.push(node.val)
-    }
+//         result.push(node.val)
+//     }
     
-    dfs(root)
-    return result
+//     dfs(root)
+    const stack = [root]
+    while (stack.length > 0) {
+        const node = stack.pop()
+        if (node) {
+            result.push(node.val)
+            
+            for (let i = 0; i < node.children.length; i++) {
+                stack.push(node.children[i])
+            }
+        }
+    }
+    return result.reverse()
 };
