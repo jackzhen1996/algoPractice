a2fdcca6f860de990ceb3d1c3c10bcc53b58f64d
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-06-20 10:58:38 -0700

    Time: 114 ms (50.95%), Space: 44.8 MB (92.70%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.js
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.js
@@ -12,18 +12,32 @@
  */
 var preorder = function(root) {
     const result = []
-    const dfs = function(node) {
-        if (node == null) {
-            return
-        }
+//     const dfs = function(node) {
+//         if (node == null) {
+//             return
+//         }
         
         
-        result.push(node.val)
-        for (const child of node.children) {
-            dfs(child)
+//         result.push(node.val)
+//         for (const child of node.children) {
+//             dfs(child)
+//         }
+//     }
+    
+//     dfs(root)
+    
+    const stack = [root]
+    while (stack.length > 0) {
+        const popped = stack.pop()
+        
+        if (popped) {
+            result.push(popped.val)
+
+            // iterate over children and add to stack
+            for (let i = popped.children.length - 1; i >= 0; i--) {
+                stack.push(popped.children[i])
+            }
         }
     }
-    
-    dfs(root)
     return result
 };
