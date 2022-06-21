f47cc671d66449426f6764ff8844040ae3c28183
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-06-20 17:13:32 -0700

    Time: 128 ms (12.39%), Space: 44.7 MB (34.73%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.js
+++ 101-symmetric-tree/101-symmetric-tree.js
@@ -11,17 +11,41 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    const dfs = function(p,q) {
-        if (p == null && q == null) {
-            return true
-        }
+    // recursive
+//     const dfs = function(p,q) {
+//         if (p == null && q == null) {
+//             return true
+//         }
         
-        if (p == null || q == null || p.val !== q.val) {
+//         if (p == null || q == null || p.val !== q.val) {
+//             return false
+//         }
+        
+//         return dfs(p.right,q.left) && dfs(p.left, q.right)
+//     }
+    
+//     return dfs(root.left, root.right)
+    
+    const s1 = [root.left]
+    const s2 = [root.right]
+    
+    while (s1.length > 0 || s1.length > 0) {
+        const left = s1.pop()
+        const right = s2.pop()
+        
+        if (left == null && right == null) {
+            continue
+        } else if (right == null || left == null || left.val !== right.val) {
             return false
         }
         
-        return dfs(p.right,q.left) && dfs(p.left, q.right)
+        s1.push(left.left)
+        s1.push(left.right)
+        s2.push(right.right)
+        s2.push(right.left)
     }
     
-    return dfs(root.left, root.right)
+    
+    
+    return true
 };
