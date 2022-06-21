c47d98439e8fdba70e4ce28e4d6047edadd65a88
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-06-20 22:03:39 -0700

    Time: 68 ms (98.68%), Space: 46.5 MB (97.42%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.js
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -15,21 +15,21 @@ var isBalanced = function(root) {
     // get the max depth for each subtree, and find the difference
     // if diff > 1: set a flag to false
     let found = true
-    const dfs = function(root){
-        if (root == null) {
+    const dfs = function(node) {
+        if (node == null) {
             return 0
         }
-    
-        const leftHeight = dfs(root.left)
-        const rightHeight = dfs(root.right)
-    
-        const heightDiff = Math.abs(leftHeight - rightHeight)
-        console.log(heightDiff)
-        if (heightDiff > 1) {
+
+        const left = dfs(node.left)
+        const right = dfs(node.right)
+
+        if (Math.abs(left-right) > 1) {
             found = false
         }
-        return 1 + Math.max(leftHeight,rightHeight)
+
+        return 1 + Math.max(left,right)
     }
+    
     dfs(root)
     return found
 };
