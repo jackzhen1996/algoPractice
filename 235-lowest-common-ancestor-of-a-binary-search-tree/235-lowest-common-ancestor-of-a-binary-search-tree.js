11be01719606f515133b536ac6c1c6736e6f7687
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-06-21 23:06:48 -0700

    Time: 122 ms (45.85%), Space: 52.3 MB (49.37%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,23 +13,18 @@
  * @return {TreeNode}
  */
 
-// time: O(n/2), n = num of nodes in the tree, => O(n)
+// time: O(log n), n = num of nodes in the tree
 // space: O(1)
 var lowestCommonAncestor = function(root, p, q) {
-    // BST means that every left child must be smaller than its parents, and every right child must be bigger than its parents
-    
-    // if p and q are both less then root, then that means go find LCA on the left subtree
-    // if p and q are both greater than root, then go find LCA on right side
-    // if p is < root, q > than root or wise-versa, the LCA has to be root
-    
-    
     if (p.val > root.val && q.val > root.val) {
-        // go right
-        return lowestCommonAncestor(root.right, p, q)
-    } else if (p.val < root.val && q.val < root.val) {
-        // go left
-        return lowestCommonAncestor(root.left, p, q)
-    } else {
+        return lowestCommonAncestor(root.right,p,q)
+    } 
+    
+    if (p.val < root.val && q.val < root.val) {
+        return lowestCommonAncestor(root.left,p,q)
+    }
+    
+    if (p.val <= root.val && q.val >= root.val || p.val >= root.val && q.val <= root.val) {
         return root
     }
 };
