7c7eaf48265c5d0810ae49bc4a0354006a7bbc1b
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-06-22 22:58:10 -0700

    Time: 106 ms (68.09%), Space: 48.3 MB (84.04%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.js
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.js
@@ -12,34 +12,82 @@
  */
 var findMode = function(root) {
     // same occurences = multiple modes
-    const map = {}
-    let maxCount = 0
-    const result = []
+//     const map = {}
+//     let maxCount = 0
+//     const result = []
+    
+//     const dfs = function(node) {
+//         if (node == null) {
+//             return
+//         }
+        
+//         if (map[node.val]) {
+//             map[node.val] ++
+//         } else {
+//             map[node.val] = 1
+//         }
+        
+//         maxCount = Math.max(maxCount, map[node.val])
+        
+//         dfs(node.left)
+//         dfs(node.right)
+//     }
+    
+//     dfs(root)
+    
+//     for (const num in map) {
+//         if (map[num] === maxCount) {
+//             result.push(num)
+//         }
+//     }
+    
+//     return result
+    
+    
+    // no extra space use inorder traversal
+    
+    // think about getting the mode from a list
+    // if current is not the same as prev, count restarts
+    // if same, count ++
+    
+    // if count > max, the new mode is the max, cleaer the modes array because the old mode is smaller
+    // add the new mode
+    // if count == max, means there's another mode that is the same count as the current mode
+    let modes = []
+    let prev = null
+    let max = 0
+    // when encounter first node, count should be 1, since prev is null to begin with
+    let count = 1
+    
+    // 1 2 
     
     const dfs = function(node) {
         if (node == null) {
-            return
+            return 
         }
-        
-        if (map[node.val]) {
-            map[node.val] ++
-        } else {
-            map[node.val] = 1
-        }
-        
-        maxCount = Math.max(maxCount, map[node.val])
         
         dfs(node.left)
+        
+        if (prev != null) {
+            if (prev === node.val) {
+                count ++
+            } else {
+                count = 1
+            }
+        }
+        
+        if (count > max) {
+            max = count
+            modes = []
+            modes.push(node.val)
+        } else if (count === max) {
+            modes.push(node.val)
+        }
+        
+        prev = node.val
+        
         dfs(node.right)
     }
-    
     dfs(root)
-    
-    for (const num in map) {
-        if (map[num] === maxCount) {
-            result.push(num)
-        }
-    }
-    
-    return result
+    return modes
 };
