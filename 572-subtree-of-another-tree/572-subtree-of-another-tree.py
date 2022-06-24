9ed87a410ede6678a335c4a278ab320541b25191
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-06-23 23:31:08 -0700

    Time: 117 ms (93.14%), Space: 14.6 MB (94.08%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -1,57 +1,35 @@
-from collections import deque
-
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-def isSame(root, subRoot):
-    # base case
-    
-    if root is None and subRoot is None:
-        return True
-    
-    if root is None or subRoot is None:
-        return False
-    
-    if root.val != subRoot.val:
-        return False
-    
-    return isSame(root.left, subRoot.left) and isSame(root.right, subRoot.right)
-
-class Solution(object):
-    def isSubtree(self, root, subRoot):
-        """
-        :type root: TreeNode
-        :type subRoot: TreeNode
-        :rtype: bool
-        """
-        if root.left is None and root.right is None and subRoot.left is None and subRoot.right is None:
-            return root.val == subRoot.val
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        # dfs on the tree to find the subroot
+        # then perform another dfs to see if same tree
         
-        q = deque([root])
-        foundSubTree = False
-        while len(q) > 0:
-            if foundSubTree:
+        def isSameTree(a,b):
+            if not a and not b:
+                return True
+            elif not a or not b or a.val != b.val:
+                return False
+            
+            return isSameTree(a.left, b.left) and isSameTree(a.right, b.right)
+        
+        stack = [root]
+        while len(stack) > 0:
+            node = stack.pop()
+            
+            if node.val == subRoot.val and isSameTree(node,subRoot):
                 return True
             
-            # pop the first one off the queue                
-            # add its chlidren to queue
+            if node:
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+                    
             
-            # compare root val and subroot val
-                # if same: use isSame()
-            currentRoot = q.popleft()
-            
-            if currentRoot.val == subRoot.val:
-                foundSubTree = isSame(currentRoot, subRoot)
-            
-            if currentRoot.left is not None:
-                q.append(currentRoot.left)
-            
-            if currentRoot.right is not None:
-                q.append(currentRoot.right)
+        return False
         
-        return foundSubTree
-            
